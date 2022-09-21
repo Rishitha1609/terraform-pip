2ab35c17c9e138fa52a2075e69ccab80ed4304e4
rishitha <rishithadevops16@gmail.com>
2022-09-21 12:18:38 +0530

    modified pip

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = var.rgname
-  location = var.location
+  name     = "terraform-pip"
+  location = "east us"
 }
 
 resource "azurerm_public_ip" "example" {
-  name                = var.pipname
+  name                = "pip001"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
